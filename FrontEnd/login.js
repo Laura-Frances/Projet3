232d6625d376867fe7397151ade33f04a1f9dce4
Laura Frances <laura742@hotmail.fr>
2023-04-13 10:10:08 +0200

    delete work from modal

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,38 +1,47 @@
-// PAGE DE CONNEXION DE L'UTILISATEUR AVEC TOKEN//
+// SET THE TOKEN FOR USER CONNECTION //
 
+// we create the variables
 const apiUrl = 'http://localhost:5678/api/users/login';
 const loginForm = document.querySelector('#login-form');
 const emailInput = document.querySelector('#email');
 const passwordInput = document.querySelector('#password');
 const errorMessage = document.querySelector('#error-message');
 
-loginForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
+loginForm.addEventListener('submit', async (event) => 
+{
+  event.preventDefault(); // empêche la page de se recharger après la soumission du formulaire
 
-  const email = emailInput.value;
+  const email = emailInput.value; // on récupère les valeurs email et mdp
   const password = passwordInput.value;
 
-  try {
-    const response = await fetch(apiUrl, {
-      method: 'POST',
-      headers: {
+  try // try-catch : execute une partie du code qui peut potentiellement échouer
+  {
+    const response = await fetch(apiUrl,
+    {
+      method: 'POST',//envoie la request POST afin de vérifier si l'user a entré les info de co valides
+      headers: 
+      {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ email, password })
     });
 
-    if (response.ok) {
-      const data = await response.json();
+    if (response.ok) 
+    {
+      const data = await response.json(); // on stock les données extraites par la méthode json
       const token = data.token;
 
-      localStorage.setItem('token', token);
-
-      window.location.href = 'http://127.0.0.1:5500/index.html'; // Redirige vers la page d'accueil
-    } else {
+      localStorage.setItem('token', token); // on stock le token dans le localstorage
+      window.location.href = 'http://127.0.0.1:5500/index.html'; // Redirects to homepage
+    } 
+    else 
+    {
       const error = await response.text();
-      errorMessage.textContent = 'Les informations de l\'utilisateur ne sont pas correctes'; // Afficher un message d'erreur
+      errorMessage.textContent = 'Les informations de l\'utilisateur ne sont pas correctes';
     }
-  } catch (error) {
+  } 
+  catch (error) 
+  {
     errorMessage.textContent = error.message;
   }
 });
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,21 +1,20 @@
-
-
-//code permettant de récupérer les éléments de travail de chaque fonction, et de les afficher sur la page web lorsque celle-ci est prête... 
-//...en utilisant l'événement DOMContentLoaded afin de s'assurer que le DOM est prêt à être utilisé avant d'afficher les données.
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () 
+{
   getCategories();
   getWorks();
 });
 
-//***************************PARTIE WORKS***************************//
+//***************************WORKS PART***************************//
 
-// Récupération des works via l'API
-function getWorks() {
+// get the works from API
+function getWorks() 
+{
   fetch('http://localhost:5678/api/works')
     .then(response => response.json())
     .then((works) => {
 
-      works.forEach((work) => { //pour chaque work de la catégorie travaux...
+      works.forEach((work) => 
+      { //pour chaque work de la catégorie travaux...
         const figure = document.createElement('figure'); //...on récupère la figure
         const img = document.createElement('img'); // puis l'image
         const figcaption = document.createElement('figcaption'); // puis l'élément de légende.
@@ -33,13 +32,15 @@ function getWorks() {
     })
 }
 
-//***************************PARTIE CATEGORIES***************************//
+//***************************CATEGORY PART***************************//
 
 // Récupération des catégories via l'API
-function getCategories() {
+function getCategories() 
+{
   fetch('http://localhost:5678/api/categories')
     .then(response => response.json())
-    .then((categories) => {
+    .then((categories) => 
+    {
 
       const divCategory = document.querySelector(".category-buttons"); // on récupére la div des boutons pour le tri par catégorie
       const btnAll = document.createElement("button");  // on créé la variable pour le bouton ''Tous''
@@ -48,7 +49,8 @@ function getCategories() {
       btnAll.setAttribute('data-category', 0); // on ajoute l'attribut data-category à l'élément 'btnAll' et on lui ajoute la valeur 0.
       divCategory.appendChild(btnAll); // 'btnAll' est ajouté à l'élément divCategory
 
-      categories.forEach((category) => { // on crée les boutons pour chaque catégorie
+      categories.forEach((category) => 
+      { // on crée les boutons pour chaque catégorie
 
         const btn = document.createElement("button"); // on récupère les boutons pour le tri
 
@@ -62,13 +64,15 @@ function getCategories() {
     });
 }
 
-//***************************FONCTION DE TRI***************************//
+//***************************SORTING FUNCTION***************************//
 
-function setUpSorting() {
+function setUpSorting() 
+{
 
   const categoryButtons = document.querySelectorAll('.category-buttons button');   // on récupère les boutons de la "category-buttons"
 
-  categoryButtons.forEach(button => { // pour chaque bouton...
+  categoryButtons.forEach(button => 
+  { // pour chaque bouton...
     button.addEventListener('click', () => { //on ajoute un événement de clic...
 
       const categoryId = button.getAttribute('data-category');//...qui récupère l'id de catégorie associé au bouton
@@ -79,16 +83,20 @@ function setUpSorting() {
 
 // Fonction de tri des images par rapport à la catégorie sélectionnée
 
-function filterImages(categoryId) {
+function filterImages(categoryId) 
+{
 
   const works = document.querySelectorAll('.gallery figure'); // on récupère les figures dans la gallery de l'html
 
   works.forEach(work => { // pour chaque work...
     const workCategory = work.querySelector('img').getAttribute('data-category'); //...on séléctioonne l'img pour lui donner l'attribut data-category
 
-    if (categoryId === '0' || workCategory === categoryId) { // on vérifie si la catégorie sélectionnée est 0 (tous) ou si la catégorie correspond à une catégorie de travail
+    if (categoryId === '0' || workCategory === categoryId) 
+    { // on vérifie si la catégorie sélectionnée est 0 (tous) ou si la catégorie correspond à une catégorie de travail
       work.style.display = ''; //...si c'est le cas, alors l'élément s'affiche en utilisant display block
-    } else {
+    } 
+    else 
+    {
       work.style.display = 'none'; // ...si ce n'est pas le cas alors les works se masquent en utilisant la valeur none
     }
   });
@@ -98,55 +106,62 @@ function filterImages(categoryId) {
 //***************************INTERFACE LOGIN/LOGOUT***************************//
 
 // on stock le token dans le localStorage
-function getToken() {
+function getToken() 
+{
   myToken = localStorage.getItem('token')
   console.log(myToken)
 }
 
-let myToken = null; // définie avec une valeur par défaut
+let myToken = null; // defined with a default value
 
 document.addEventListener
   (
-    "DOMContentLoaded", // DOMContentLoaded déclenché lorsque page html chargée
-    function () {
+    "DOMContentLoaded", // DOMContentLoaded triggered when html page loaded
+    function () 
+    {
       const loginButton = document.getElementById('loginButton');
       getToken();
 
-      if (myToken != null) { // si le token se trouve dans le localstorage (n'est pas null), alors nous sommes connectés, logout apparait
+      if (myToken != null) { // if the token is not null
         loginButton.textContent = 'logout';
         document
-          .querySelectorAll('.hidden') // on sélectionne la classe hidden
-          .forEach
+          .querySelectorAll('.hidden') // we select the .hidden class
+          .forEach 
           (
-            element => // pour chaque élément on enlève l'élément hidden
+            element => // for each element we remove the hidden element
             {
               element.classList.remove('hidden');
             }
           )
         document.querySelector('.category-buttons').classList.add('hidden');
       }
-      else { // autrement les éléments restent en hidden et le 'login' s'affiche
+      else 
+      { // otherwise the elements remain hidden and the 'login' appears
         loginButton.textContent = 'login';
         document
           .querySelectorAll('.hidden')
           .forEach
           (
-            element => {
+            element => 
+            {
               element.classList.add('hidden');
             }
           )
         document.querySelector('.category-buttons').classList.remove('hidden');
       }
 
-      // événement click ajouté au bouton pour gérer la connexion/déconnexion de l'user
-      loginButton.addEventListener('click', () => {
-        if (myToken) {
+      // click event added to button to manage user login/logout
+      loginButton.addEventListener('click', () => 
+      {
+        if (myToken) 
+        {
           localStorage
             .removeItem('token');
           loginButton.textContent = 'login';
           window.location.href = "http://127.0.0.1:5500/index.html"
         }
-        else {
+        else 
+        {
           loginButton.textContent = 'logout';
           window.location.replace("http://127.0.0.1:5500/login.html")
         }
@@ -154,70 +169,115 @@ document.addEventListener
     }
   );
 
-//***************************AJOUT DE LA MODALE***************************//
+//***************************GALLERY MODALE***************************//
 
-// on récupère les éléments <a>, xmark et modale  //
+// get the elements <a>, xmark and modal  //
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () 
+{
   const modalOpen = document.querySelector('.modal-open');
   const modalClose = document.querySelector('.modal-close');
   const modal = document.querySelector('#modal-gallery');
   const modalBackdrop = document.getElementById('modal-backdrop'); // background modal
 
-  modalOpen.onclick = function () {
+  modalOpen.onclick = function () 
+  {
     modal.setAttribute('aria-hidden', 'false');
     modalBackdrop.style.display = 'block'; // show the background
-    modal.style.display = 'flex'; 
-    getModal(); // on récupère la fonction getModal qui affiche les works
+    modal.style.display = 'flex';
+    getModal();
   }
 
-  modalClose.onclick = function () {
+  modalClose.onclick = function () 
+  {
     modal.setAttribute('aria-hidden', 'true');
-    modalBackdrop.style.display = 'none'; 
+    modalBackdrop.style.display = 'none';
     modal.style.display = 'none';
-    clearModal(); // on vide l'intérieur de la modal en appelant la fonction clearModal
+    clearModal(); // call the function clearModal
   }
-
-  modal.onclick = function (event) {
-    event.stopPropagation();
-  }
+  // modal.onclick = function (event) // 
+  // {
+  //   event.stopPropagation();
+  // }
 });
 
-// INSER CLOSE MODALE WHEN CLICK OUTSIDE 
+// INSER CODE FOR CLOSE MODALE WHEN CLICK OUTSIDE here ...
 
-
-// récupérer à nouveau les works via l'API //
-function getModal() {
+// GET THE WORKS AGAIN FROM API  //
+function getModal() 
+{
   fetch('http://localhost:5678/api/works')
     .then(response => response.json())
-    .then((modal) => {
+    .then((modal) => 
+    {
 
-      modal.forEach((modal) => { 
-        const figure = document.createElement('figure'); 
-        const img = document.createElement('img'); 
+      modal.forEach((modal) => 
+      {
+        const figure = document.createElement('figure');
+        const img = document.createElement('img');
         const figcaption = document.createElement('figcaption');
         const trashIcon = document.createElement('i');
         const arrowIcon = document.createElement('i')
 
-        img.src = modal.imageUrl; 
-        img.alt = modal.title; 
-        img.setAttribute('data-img', modal.categoryId); 
-        figcaption.innerText = "éditer"; 
+        img.src = modal.imageUrl;
+        img.alt = modal.title;
+        img.setAttribute('data-img', modal.categoryId);
+        figcaption.innerText = "éditer";
 
-        arrowIcon.className= "fa-solid fa-arrows-up-down-left-right arrow-icon";
+        arrowIcon.className = "fa-solid fa-arrows-up-down-left-right arrow-icon";
         trashIcon.className = "fa-regular fa-trash-can trash-icon";
+
         figure.appendChild(img);
         figure.appendChild(trashIcon);
-        figure.appendChild(figcaption); 
+        figure.appendChild(figcaption);
         figure.appendChild(arrowIcon);
+
+        trashIcon.addEventListener('click', () => 
+        {
+          deleteImage(modal.id, figure); //call function deleteImage
+        });
 
         document.querySelector('.modal-container').appendChild(figure);
       });
     })
 }
-function clearModal() {
+
+// DELETE ITEM
+
+function deleteImage(id, figure) // sends a delete request to the server to delete the img with a specified id and figure
+{
+
+  fetch(`http://localhost:5678/api/works/${id}`, 
+  {
+    method: 'GET',
+    headers:
+    {
+      'Authorization': `Bearer ${myToken}`
+    }
+  })
+    .then(data => data.json)
+    .then(jsondata => {
+      console.log(jsondata.work);
+    })
+    .then(response => 
+    {
+      alert('id = ' + id + ' , response = ' + response.status)
+
+      if (response.ok) {
+        alert('suppression');
+        figure.remove(); // remove image from DOM
+      }
+    });
+}
+
+function clearModal() // empty the modal 
+{
   const modalContainer = document.querySelector('.modal-container');
-  while (modalContainer.firstChild) {
-    modalContainer.removeChild(modalContainer.firstChild);
+  while (modalContainer.firstChild) 
+  {
+    modalContainer.removeChild(modalContainer.firstChild); // remove all children of the modalContainer element
+    window.location.href = ('http://127.0.0.1:5500/index.html'); // Redirects to homepage
   }
 }
+
+//***************************MODAL ADD PHOTO***************************//
